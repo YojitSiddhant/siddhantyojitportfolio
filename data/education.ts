ec59291231307c9125cb587a935487901e2eef4a
Siddhant Yojit <sid.mailconnect@gmail.com>
2026-07-29 15:10:05 +0530

    Education Page Update

--- data/education.ts
+++ data/education.ts
@@ -1,30 +1,22 @@
-export const education = [
+export type EducationItem = {
+  institute: string;
+  degree: string;
+  duration: string;
+  description?: string;
+  logo: string;
+};
+
+export const education: EducationItem[] = [
   {
     institute: "PES University",
-    degree: "MCA",
+    degree: "Master of Computer Applications (MCA)",
     duration: "2024 - 2026",
-    description: "CGPA: 6.27/10",
     logo: "/company-logos/pes-logo.webp",
   },
   {
     institute: "New Horizon College",
-    degree: "BCA",
+    degree: "Bachelor of Computer Applications (BCA)",
     duration: "2020 - 2023",
-    description: "CGPA: 7.59/10",
     logo: "/company-logos/nhcm-logo.jpeg",
-  },
-  {
-    institute: "St Francis PU College",
-    degree: "Class XII",
-    duration: "2019 - 2020",
-    description: "61.33%",
-    logo: "/company-logos/st-francis-pu-college-logo.jpeg",
-  },
-  {
-    institute: "Narayana e-Techno School",
-    degree: "Class X",
-    duration: "2016 - 2017",
-    description: "CGPA: 9.5/10",
-    logo: "/company-logos/narayana-e-techno-school.jpeg",
   },
 ];
